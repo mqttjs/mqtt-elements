3f12ae59d68670556a34c0bfdcdedc03885cb9df
Sandro Kock <sandro.kock@gmail.com>
2016-10-28 10:16:09 +0200

    removing test-coverage to get test running
    https://github.com/Polymer/web-component-tester/issues/373

--- wct.conf.js
+++ wct.conf.js
@@ -1,18 +1,6 @@
 module.exports = {
   "verbose": true,
   "plugins": {
-    "istanbul": {
-      "dir": "./coverage",
-      "reporters": [
-        "text-summary",
-        "lcov",
-        "html"
-      ],
-      "include": [
-        "mqtt-connection.html"
-      ],
-      "exclude": []
-    },
     "local": {
       "skipSeleniumInstall": false,
       "persistent": true,
